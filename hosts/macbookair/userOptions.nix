ffc8bfa9c33dbbb243103e0b111a56222f7d1ff4
Javier Arrieta <javier@techdelivery.es>
2025-12-15 20:15:18 +0100

    fix macbook air home folder name

--- hosts/macbookair/userOptions.nix
+++ hosts/macbookair/userOptions.nix
@@ -1,6 +1,6 @@
 {
   username = "javier";
-  userHome = "/home/javier";
+  userHome = "/Users/javier";
   gitName = "Javier Arrieta";
   gitEmail = "javier@techdelivery.es";
   gitDefaultBranch = "main";
