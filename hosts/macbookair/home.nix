354546f5fc2b3161168577ad3ed6b5fbdac54a0e
Javier Arrieta <javier@techdelivery.es>
2026-02-07 18:11:57 +0100

    add autocomplete server

--- hosts/macbookair/home.nix
+++ hosts/macbookair/home.nix
@@ -20,7 +20,10 @@ in
     shellAliases = {
       "hm-apply" = "home-manager switch --flake ${userOptions.homeManagerConfigDir}#macbookair";
       "sshk" = "ssh-add -D && ssh-add -t 18h";
+      "autocomplete-server" =
+        "llama-server -m $HOME/llm/models/sweep-next-edit-1.5b.q8_0.v2.gguf --offline --jinja -ngl 99 --threads -1 --ctx-size 8192";
     };
+
   };
   home.packages = [
     pkgs.nmap
