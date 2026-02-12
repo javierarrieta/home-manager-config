66ada083c4021bd6be5a0ca5e41490a8c8ebd5c1
Javier Arrieta <javier.arrieta@oracle.com>
2026-02-12 10:49:55 +0100

    Add programming fonts

--- modules/term.nix
+++ modules/term.nix
@@ -6,10 +6,21 @@
   ...
 }:
 {
+  home.packages = with pkgs.nerd-fonts; [
+    fira-code
+    zed-mono
+    iosevka
+    jetbrains-mono
+  ];
+
   programs.kitty = {
     enable = true;
     shellIntegration = {
       enableFishIntegration = true;
+    };
+    font = {
+      name = "FiraCode-Retina";
+      size = 10;
     };
     enableGitIntegration = true;
   };
